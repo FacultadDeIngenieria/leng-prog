0dc31ed03e23c3322633b935fcebb2e3fd2b57a8
Emilio Lopez Gabeiras <emiliolg@mac.com>
2022-07-29 11:55:02 -0300

    Remove solutions from exercise

--- exercises/fraction/src/Fraction.hs
+++ exercises/fraction/src/Fraction.hs
@@ -5,28 +5,24 @@ type Fraction = (Int, Int)
 -- Implement the `add` Function
 
 add :: Fraction -> Fraction -> Fraction
-add f1 f2 = (6, 8)
+add f1 f2 = error "Implement it"
 
 -- Implement the `sub` Function
 
 sub :: Fraction -> Fraction -> Fraction
-sub f1 f2 = (0, 0)
+sub f1 f2 = error "Implement it"
 
 -- Implement the `mul` Function
 
 mul :: Fraction -> Fraction -> Fraction
-mul f1 f2 = (0, 0)
+mul f1 f2 = error "Implement it"
 
 -- Implement the `divide` Function
 
 divide :: Fraction -> Fraction -> Fraction
-divide f1 f2 = (0, 0)
+divide f1 f2 = error "Implement it"
 
 -- Implement the highest common factor (hcf) (A.K.A greatest common divisor)
 
 hcf :: Int -> Int -> Int
-hcf a b
-    |   a == b    =a
-    |   a > b     = hcf (a-b) b
-    |   otherwise = hcf a    (b-a)
-  
+hcf f1 f2 = error "Implement it"
